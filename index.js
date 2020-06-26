be4609ef7eb5a3be8b9ae6ff9cbac50d89dc0d9a
C0rqScr3W <kiddcryro@gmail.com>
2020-06-25 18:57:41 -0600

    added PORT env variable to start the server at

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
- 
+const port = process.env.PORT || 3000
 app.get('/', function (req, res) {
   res.send('GOATS ROCK')
 })
- 
-app.listen(3000)
+console.log("expresss webserver starting up at port:", port)
+app.listen(port)
